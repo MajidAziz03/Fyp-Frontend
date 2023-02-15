e08ccdb68fab93b4fae983d0af0432ba984b69cc
Majid Aziz <azizmajid34@gmail.com>
2023-02-15 11:00:51 +0500

    ADded Forecast

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -12,20 +12,20 @@ import {
 } from 'recharts';
 
 const data01 = [
-    { x: 100, y: 200, z: 200 },
-    { x: 120, y: 100, z: 260 },
-    { x: 170, y: 300, z: 400 },
-    { x: 140, y: 250, z: 280 },
-    { x: 150, y: 400, z: 500 },
-    { x: 110, y: 280, z: 200 },
+    { x: 1000, y: 3000, z: 200 },
+    { x: 1200, y: 2500, z: 260 },
+    { x: 1700, y: 4000, z: 400 },
+    { x: 1400, y: 4300, z: 280 },
+    { x: 1500, y: 5500, z: 500 },
+    { x: 1100, y: 7800, z: 200 }
 ];
 const data02 = [
-    { x: 200, y: 260, z: 240 },
-    { x: 240, y: 290, z: 220 },
-    { x: 190, y: 290, z: 250 },
-    { x: 198, y: 250, z: 210 },
-    { x: 180, y: 280, z: 260 },
-    { x: 210, y: 220, z: 230 },
+    { x: 2000, y: 3000, z: 200 },
+    { x: 3200, y: 2500, z: 260 },
+    { x: 4700, y: 4000, z: 400 },
+    { x: 5400, y: 4300, z: 280 },
+    { x: 5900, y: 5500, z: 500 },
+    { x: 5600, y: 7800, z: 200 }
 ];
 
 
@@ -44,12 +44,12 @@ function Forcast() {
                 >
                     <CartesianGrid />
                     <XAxis type="number" dataKey="x" name="Quaterly" />
-                    <YAxis type="number" dataKey="y" name="Year" />
+                    <YAxis type="number" dataKey="y" name="Profit" />
                     <ZAxis type="number" dataKey="z" range={[60, 400]} name="profit" unit="$" />
                     <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                     <Legend />
-                    <Scatter name="Containers" data={data01} fill="#8884d8" shape="star" />
-                    <Scatter name="Clients" data={data02} fill="#82ca9d" shape="triangle" />
+                    <Scatter name="Quaterly" data={data01} fill="#8884d8" shape="star" />
+                    <Scatter name="Profit" data={data02} fill="#82ca9d" shape="triangle" />
                 </ScatterChart>
             </ResponsiveContainer>
         </div>
@@ -57,3 +57,4 @@ function Forcast() {
 }
 
 export default Forcast;
+
